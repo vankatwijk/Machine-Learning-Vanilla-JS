2bd64b42833aede3e7ecb1f0640c46c0241bf74d
vankatwijk <hendrikus.hpvk@gmail.com>
2020-08-26 12:24:52 +0200

    interface change

--- script.js
+++ script.js
@@ -4,28 +4,24 @@ const net = new brain.NeuralNetwork()
 //     hiddenLayers:[4, 5,6]
 // })
 
-//x or operations
-net.train([
-    {
-        input:[0,0],
-        output:[0]
-    },
-    {
-        input:[1,0],
-        output:[1]
-    },
-    {
-        input:[0,1],
-        output:[1]
-    },
-    {
-        input:[1,1],
-        output:[0]
-    },
-])
 
-const diagram = document.getElementById('diagram')
-diagram.innerHTML = brain.utilities.toSVG(net)
+const data =[
+    {
+        input:{r:0,g:0,b:0},
+        output:[1]
+    },
+    {
+        input:{r:1,g:1,b:1},
+        output:[0]
+    }
+]
+
+
+//x or operations
+net.train(data)
+
+// const diagram = document.getElementById('diagram')
+// diagram.innerHTML = brain.utilities.toSVG(net)
 
 //test if it works with an input
-console.log(net.run([1,0]))
+console.log(net.run({r:1, g:1,b:0}))
